838e2ff211f989818db615531ca2ddfdd6177e34
AndruObraen <andru.okpako@gmail.com>
2023-02-18 12:42:03 +0100

    script that print file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 /**
-*main - entry point
-*return: always 0 (success)
-*/
+ *main - Entry point
+ *
+ *Return: always 0 (Success)
+ *Description:  A column of asterisks on the left side,
+ *with beginning and ending almost-blank lines.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
